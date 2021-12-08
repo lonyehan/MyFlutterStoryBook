efdef18925ce56a8917de0e9864910b94771c563
lonyehan <s105321019@mail1.ncnu.edu.tw>
2021-12-09 02:58:47 +0800

    新增 第二天的prototype

--- lib/pages/DropdownMenu.dart
+++ lib/pages/DropdownMenu.dart
@@ -1,5 +1,5 @@
 // ignore_for_file: file_names
-
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 class DropdownMenu extends StatelessWidget {
@@ -9,21 +9,212 @@ class DropdownMenu extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: Text("DropdownMenu")),
-        body: Center(
-            child: Row(
-          mainAxisAlignment: MainAxisAlignment.end,
+        backgroundColor: Color(0xFFECEFF8),
+        body: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
                 children: [
-                  Row(
-                    children: [Menu()],
+                  Menu(),
+                  SizedBox(
+                    height: 5,
                   ),
-                  Row(),
+                  Content(),
                 ],
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center)
+                mainAxisAlignment: MainAxisAlignment.center),
           ],
-        )));
+        ));
+  }
+}
+
+class Content extends StatelessWidget {
+  const Content({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 42, vertical: 57),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Help",
+                        style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'Roboto')),
+                    SizedBox(height: 22),
+                    Card(
+                        info: CardInfo(
+                            color: Color(0xFFFFEFF3),
+                            des:
+                                "Learn how to sign up, install and use our products.",
+                            name: "Help Center",
+                            icon: Icons.import_contacts_outlined)),
+                    SizedBox(height: 15),
+                    Card(
+                        info: CardInfo(
+                            color: Color(0xFFEFF2FF),
+                            des: "Learn, share and connect with other users.",
+                            name: "Coimmunity Forum",
+                            icon: Icons.people_outline)),
+                  ],
+                ),
+                SizedBox(width: 10),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Learn",
+                        style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'Roboto')),
+                    SizedBox(height: 22),
+                    Card(
+                        info: CardInfo(
+                            color: Color(0xFFE9FBED),
+                            des:
+                                "Learn how to sign up, install and use our products.",
+                            name: "Podcasts",
+                            icon: Icons.headset_outlined)),
+                    SizedBox(height: 15),
+                    Card(
+                        info: CardInfo(
+                            color: Color(0xFFFBF3E3),
+                            des: "Learn, share and connect with other users.",
+                            name: "Coimmunity Forum",
+                            icon: Icons.question_answer_outlined)),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10), color: Colors.white),
+        ),
+        Container(
+          color: Color(0xFFECEFF8),
+          child: SizedBox(
+            width: 91,
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class CardInfo {
+  IconData icon;
+  String name;
+  String des;
+  Color color;
+
+  CardInfo(
+      {required this.icon,
+      required this.name,
+      required this.des,
+      this.color = Colors.white});
+}
+
+class Card extends StatefulWidget {
+  CardInfo info;
+  bool bSelect;
+
+  Card({required this.info, this.bSelect = false});
+
+  @override
+  State<Card> createState() => _CardState();
+}
+
+class _CardState extends State<Card> {
+  void exitFunction(PointerExitEvent event) {
+    setState(() {
+      this.widget.bSelect = false;
+    });
+  }
+
+  void hoverFunction(PointerHoverEvent event) {
+    setState(() {
+      this.widget.bSelect = true;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MouseRegion(
+      onHover: hoverFunction,
+      onExit: exitFunction,
+      child: InkWell(
+        onTap: () => {},
+        child: Container(
+          width: 308,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10.0),
+            color: this.widget.bSelect == true
+                ? this.widget.info.color
+                : Colors.white,
+          ),
+          child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 10),
+            child: Row(
+              children: [
+                SizedBox(
+                  width: 10,
+                ),
+                Column(
+                  children: [
+                    CircleAvatar(
+                        radius: 20,
+                        backgroundColor: this.widget.info.color,
+                        child: Icon(
+                          widget.info.icon,
+                          color: Colors.black,
+                        )),
+                    SizedBox(height: 31)
+                  ],
+                ),
+                SizedBox(
+                  width: 15,
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        height: 17,
+                      ),
+                      Text("${widget.info.name}",
+                          style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'Roboto')),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        "${widget.info.des}",
+                        softWrap: true,
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  width: 30,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
 
@@ -39,7 +230,7 @@ class MenuItem extends StatefulWidget {
 }
 
 class _MenuItemState extends State<MenuItem> {
-  final bool _flag;
+  bool _flag;
   final String _name;
 
   late IconData _icon;
@@ -52,46 +243,59 @@ class _MenuItemState extends State<MenuItem> {
   @override
   void initState() {
     super.initState();
-    switch (_flag) {
-      case true:
-        setState(() {
-          _icon = Icons.keyboard_arrow_down_sharp;
-          _color = Colors.white;
-          _backgroundColor = Colors.black;
-        });
-        break;
-      case false:
-        setState(() {
-          _icon = Icons.keyboard_arrow_up_sharp;
-          _color = Colors.black;
-          _backgroundColor = Colors.white;
-        });
-        break;
-    }
+
+    setState(() {
+      _icon = Icons.keyboard_arrow_up_sharp;
+      _color = Colors.black;
+      _backgroundColor = Color(0xFFECEFF8);
+    });
+  }
+
+  void exitFunction(PointerExitEvent event) {
+    setState(() {
+      _icon = Icons.keyboard_arrow_up_sharp;
+      _color = Colors.black;
+      _backgroundColor = Color(0xFFECEFF8);
+    });
+  }
+
+  void hoverFunction(PointerHoverEvent event) {
+    setState(() {
+      _icon = Icons.keyboard_arrow_down_sharp;
+      _color = Colors.white;
+      _backgroundColor = Colors.black;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        height: 45,
-        child: Row(
-          children: [
-            SizedBox(width: 15),
-            Text(
-              _name,
-              style:
-                  TextStyle(fontSize: 16, fontFamily: 'roboto', color: _color),
+    return MouseRegion(
+      onHover: hoverFunction,
+      onExit: exitFunction,
+      child: InkWell(
+        onTap: () => {},
+        child: Container(
+            height: 45,
+            child: Row(
+              children: [
+                SizedBox(width: 15),
+                Text(
+                  _name,
+                  style: TextStyle(
+                      fontSize: 16, fontFamily: 'roboto', color: _color),
+                ),
+                Icon(
+                  _icon,
+                  color: _color,
+                ),
+              ],
             ),
-            Icon(
-              _icon,
-              color: _color,
-            ),
-          ],
-        ),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          color: _backgroundColor,
-        ));
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: _backgroundColor,
+            )),
+      ),
+    );
   }
 }
 
